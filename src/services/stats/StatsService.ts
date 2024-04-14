f808e45d93ecc6fe6dd7063963f59e569540ffce
Anna Yuova <yuova.a@gmail.com>
2024-04-14 16:57:12 +0200

    first commit

---
+++ src/services/stats/StatsService.ts
@@ -0,0 +1,92 @@
+import type { AxiosError } from 'axios';
+import type { CountStat } from 'src/contracts';
+import { api } from 'src/boot/axios';
+
+class StatsService {
+  async breedStats(): Promise<CountStat[] | null> {
+    return api
+      .get('stats/breeds')
+      .then((response) => response.data)
+      .catch((error: AxiosError) => {
+        if (error.response?.status === 401) {
+          return null;
+        }
+
+        return Promise.reject(error);
+      });
+  }
+  async yearStats(): Promise<CountStat[] | null> {
+    return api
+      .get('stats/year')
+      .then((response) => response.data)
+      .catch((error: AxiosError) => {
+        if (error.response?.status === 401) {
+          return null;
+        }
+
+        return Promise.reject(error);
+      });
+  }
+  async countryStats(): Promise<CountStat[] | null> {
+    return api
+      .get('stats/country')
+      .then((response) => response.data)
+      .catch((error: AxiosError) => {
+        if (error.response?.status === 401) {
+          return null;
+        }
+
+        return Promise.reject(error);
+      });
+  }
+
+  async getYears(): Promise<{ years: number[] }> {
+    return api
+      .get('stats/getCatYears/')
+      .then((response) => {
+        return response.data;
+      })
+      .catch((error: AxiosError) => {
+        if (error.response?.status === 401) {
+          return null;
+        }
+
+        return Promise.reject(error);
+      });
+  }
+
+  async getBreedCount(year: number): Promise<{
+    dictionary: any;
+  }> {
+    return api
+      .post('stats/getBreedCount/', { year: year })
+      .then((response) => {
+        return response.data;
+      })
+      .catch((error: AxiosError) => {
+        if (error.response?.status === 401) {
+          return null;
+        }
+
+        return Promise.reject(error);
+      });
+  }
+  async getBreedYearCount(breeds: string[]): Promise<{
+    dictionary: any;
+  }> {
+    return api
+      .post('stats/getBreedYearCount/', { breeds: breeds })
+      .then((response) => {
+        return response.data;
+      })
+      .catch((error: AxiosError) => {
+        if (error.response?.status === 401) {
+          return null;
+        }
+
+        return Promise.reject(error);
+      });
+  }
+}
+
+export default new StatsService();
